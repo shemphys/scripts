36cf0f4569d456b1f0debebe6bdea24b789eb81e
Miguel Paredes <mykjestic@gmail.com>
2023-05-22 12:51:43 +0200

    ahora también colorea jajaja

--- mouse_sensitivity.py
+++ mouse_sensitivity.py
@@ -1,9 +1,11 @@
 import ctypes
+from colorama import init, Fore, Style
 
-def obtener_sensibilidad_actual():
+#No soy capaz de que coja bien la sensibilidad inicial del mouse...
+'''def obtener_sensibilidad_actual():
     user32 = ctypes.windll.user32
     current_sensitivity = user32.SystemParametersInfoW(113, 0, None, 0)
-    return current_sensitivity
+    return current_sensitivity'''
 
 def cambiar_sensibilidad(nueva_sensibilidad):
     user32 = ctypes.windll.user32
@@ -11,10 +13,13 @@ def cambiar_sensibilidad(nueva_sensibilidad):
     print("La sensibilidad del ratón se ha cambiado a", nueva_sensibilidad)
 
 def main():
-    print("La sensibilidad actual del ratón es:", obtener_sensibilidad_actual())
-    print()
+    init()  # Inicializar colorama
+    #print(Fore.RED + "La sensibilidad actual del ratón es:", obtener_sensibilidad_actual())
+    print(Style.RESET_ALL)  # Restablecer el color al predeterminado
 
     while True:
+        print(Fore.RED + "Miguel usa una sensibilidad de 4", Style.RESET_ALL)
+        print(Fore.BLUE + "Menci usa una sensibilidad de 400", Style.RESET_ALL)
         nueva_sensibilidad = input("Ingrese un número entre 0 y 20 para establecer la sensibilidad del ratón (o 'q' para salir): ")
         if nueva_sensibilidad.lower() == 'q':
             break
@@ -24,9 +29,9 @@ def main():
             if 0 <= nueva_sensibilidad <= 20:
                 cambiar_sensibilidad(nueva_sensibilidad)
             else:
-                print("El número ingresado está fuera del rango válido (0-20).")
+                print("El número ingresado está fuera del rango válido (0-20).\n")
         except ValueError:
-            print("Entrada no válida. Por favor, ingrese un número válido.")
+            print("Entrada no válida. Por favor, ingrese un número válido.\n")
 
 if __name__ == '__main__':
     main()
